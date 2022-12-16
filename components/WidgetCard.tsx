2a70535990ae7db15f19169e3fc13cd1e08f0b19
aniebodavid <aniebovictor001@gmail.com>
2022-12-16 15:01:08 -0800

    feat:: implement widget card component

--- components/WidgetCard.tsx
+++ components/WidgetCard.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import Image from "next/image"
+import ArrowRise from "./assets/ArrowRise.svg"
 
 type WidgetCardProps = {
   name: string
@@ -9,13 +11,17 @@ type WidgetCardProps = {
 
 const WidgetCard = () => {
   return (
-    <div className="w-64 border border-1 border-red-600 rounded-lg p-6">
+    <div className="w-80 rounded-3xl p-10 bg-sky-100 m-4">
       <h4 className="font-semibold text-black text-xl ">Customers</h4>
-      <div className="flex border border-1 border-red-600 justify-between text-center align-center mt-4">
+      <div className="flex justify-between text-center align-center mt-4">
         <p className="font-bold text-4xl">3,781</p>
 
-        <p className="self-center font-medium text-xl">+5.27%</p>
-        <span></span>
+        <div className="flex gap-2">
+          <p className="self-center font-medium text-xl">+5.27%</p>
+          <span className="self-center">
+            <Image src={ArrowRise} alt={ArrowRise} width={25} height={25} />
+          </span>
+        </div>
       </div>
     </div>
   )
